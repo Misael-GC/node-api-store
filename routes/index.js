0382cb77186e03f5edd5126d97f793b54072dad6
Misael-GC <misael.gomez1623@gmail.com>
2024-09-09 21:29:07 -0600

    agregando un estandar para la ruta padre /api/v1/

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
+const express = require('express');
 const productsRouter = require('./products.routes.js');
 const usersRouter = require('./users.routes.js');
 const categoriesRouter = require('./categories.routes.js');
 
 
 function routerApi(app){
-  app.use('/products', productsRouter);
-  app.use('/users', usersRouter);
-  app.use('/categories', categoriesRouter);
+
+  const router = express.Router();
+  app.use('/api/v1', router);
+  router.use('/products', productsRouter);
+  router.use('/categories', categoriesRouter);
+  router.use('/users', usersRouter);
 }
 
 module.exports = routerApi;  //exportando o módulo para ser usado em outros ar
